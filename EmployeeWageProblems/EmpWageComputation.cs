6fe15d20c6f5f688cbb4310a975fcd0eeed27984
Biru4015 <rambirendra12@gmail.com>
2020-04-24 18:09:04 +0530

    [Birendra]Add. added EmpWageComputation.cs file with welcome message

---
+++ EmployeeWageProblems/EmpWageComputation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeWageProblems
+{
+    /// <summary>
+    /// This conatains the code for welcome message for Employee wage problem
+    /// </summary>
+    class EmpWageComputation
+    {
+        /// <summary>
+        /// This is main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocme to Employee wage Problem");
+        }
+    }
+}
