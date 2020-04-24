bab026c316b2a959167fc1fe3803e657d7b3b2d1
Biru4015 <rambirendra12@gmail.com>
2020-04-24 22:43:23 +0530

    [Birendra]Refactor. refactored and getting working hour using function

--- EmployeeWageProblems/EmpWageComputation.cs
+++ EmployeeWageProblems/EmpWageComputation.cs
@@ -13,6 +13,34 @@ namespace EmployeeWageProblems
         public const int EMP_WAGE_PER_HR = 20;
         public const int WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
+
+        //varialble
+        public static int totalEmpHrs = 0;
+        public static int totalWorkingDays = 0;
+        public static int empHr = 0;
+
+        /// <summary>
+        /// This method is created for getting employee working hour
+        /// </summary>
+        /// <returns>empHrs</returns>
+        public static int GetWorkingHour(int rand)
+        {
+            int empHrs = 0;
+            switch(rand)
+            {
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            return empHrs;
+        }
+        
         /// <summary>
         /// This is main method
         /// </summary>
@@ -21,30 +49,16 @@ namespace EmployeeWageProblems
         {
             Console.WriteLine("Welocme to Employee wage Problem");
             int monthlySalary = 0;
-            int empHr=0;
-            int totalEmpHrs = 0;
-            int totalWorkingDay = 0;
-            while(totalEmpHrs < MAX_HRS_IN_MONTH && totalEmpHrs < WORKING_DAYS)
+            while(totalWorkingDays < MAX_HRS_IN_MONTH && totalWorkingDays < WORKING_DAYS)
             {
-                totalWorkingDay++;
+                totalWorkingDays++;
                 Random random = new Random();
                 int rand = (random.Next() % 3) + 1;
-                switch(rand)
-                {
-                    case IS_PART_TIME:
-                        empHr = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHr = 8;
-                        break;
-                    default:
-                        empHr = 0;
-                        break;
-                }
+                empHr = GetWorkingHour(rand);
                 totalEmpHrs = totalEmpHrs + empHr;
             }
             monthlySalary = EMP_WAGE_PER_HR * totalEmpHrs;
-            Console.WriteLine("Monthly salary of employee is "+monthlySalary);
+            Console.WriteLine("Total salary of employee in a month is "+monthlySalary);
         }
     }
 }
