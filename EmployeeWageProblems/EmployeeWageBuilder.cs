86ad74c78dff2ac22670933962b6a9697a797743
Biru4015 <rambirendra12@gmail.com>
2020-04-25 22:47:58 +0530

    [Birendra]Add. added paramter constructor for save the total wage for each company

--- EmployeeWageProblems/EmployeeWageBuilder.cs
+++ EmployeeWageProblems/EmployeeWageBuilder.cs
@@ -16,14 +16,35 @@ namespace EmployeeWageProblems
         public static int totalWorkingDays = 0;
         public static int empHr = 0;
 
+        //variable
+        private String companyName;
+        private int empRate;
+        private int numOfDays;
+        private int maxHrs;
+
+        /// <summary>
+        /// Initialize the paramters constructor
+        /// </summary>
+        /// <param name="companyName"></param>
+        /// <param name="empRate"></param>
+        /// <param name="numOfDays"></param>
+        /// <param name="maxHrs"></param>
+        public EmployeeWageBuilder(String companyName,int empRate,int numOfDays,int maxHrs)
+        {
+            this.companyName = companyName;
+            this.empRate = empRate;
+            this.numOfDays = numOfDays;
+            this.maxHrs = maxHrs;
+        }
+
         /// <summary>
         /// This method is created for getting employee working hour
         /// </summary>
         /// <returns>empHrs</returns>
-        public static int GetWorkingHour(int rand)
+        public static int GetWorkingHour(int checkAttendace)
         {
             int empHrs = 0;
-            switch(rand)
+            switch(checkAttendace)
             {
                 case IS_PART_TIME:
                     empHrs = 4;
@@ -41,19 +62,19 @@ namespace EmployeeWageProblems
         /// <summary>
         /// This methos is created for calculate employee wage
         /// </summary>
-        public static void ComputeEmployeeWage(String company,int empRate,int numOFDays,int maxHrs )
+        public void ComputeEmployeeWage()
         {
             int monthlySalary = 0;
-            while (totalEmpHrs<maxHrs  && totalWorkingDays < numOFDays)
+            while (totalEmpHrs<maxHrs  && totalWorkingDays < numOfDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
-                int rand = (random.Next() % 3) + 1;
-                empHr = GetWorkingHour(rand);
+                int checkAttendance = (random.Next() % 3) + 1;
+                empHr = GetWorkingHour(checkAttendance);
                 totalEmpHrs = totalEmpHrs + empHr;
             }
             monthlySalary = empRate * totalEmpHrs;
-            Console.WriteLine("Day "+totalWorkingDays+" Employee of a company is "+company+" is "+monthlySalary);
+            Console.WriteLine("Day "+totalWorkingDays+" Employee of a company is "+companyName+" is "+monthlySalary);
         }
 
         /// <summary>
@@ -63,10 +84,14 @@ namespace EmployeeWageProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Welocme to Employee wage Problem");
-            ComputeEmployeeWage("BridgeLabz",25,22,90);
-            ComputeEmployeeWage("Flipkart", 40, 18, 100);
-            ComputeEmployeeWage("Amazon", 45, 24, 110);
-            ComputeEmployeeWage("ThoughtWorks", 30, 20, 110);
+            EmployeeWageBuilder bridgelabzEmployeeWageBuilder = new EmployeeWageBuilder("BridgeLabzs",25,22,90);
+            EmployeeWageBuilder flipkartEmployeeWageBuilder = new EmployeeWageBuilder("Flipkart", 40, 18, 100);
+            EmployeeWageBuilder amazonEmployeeWageBuilder = new EmployeeWageBuilder("Amazon", 45, 24, 110);
+            EmployeeWageBuilder thoughtworksEmployeeWageBuilder = new EmployeeWageBuilder("ThoughtWorks", 30, 20, 120);
+            bridgelabzEmployeeWageBuilder.ComputeEmployeeWage();
+            flipkartEmployeeWageBuilder.ComputeEmployeeWage();
+            amazonEmployeeWageBuilder.ComputeEmployeeWage();
+            thoughtworksEmployeeWageBuilder.ComputeEmployeeWage();
         }
     }
 }
